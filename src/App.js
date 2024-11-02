e4a3d6a74b5e04f147fd2138cc035ab1031b6cba
SagalMireAbukar755 <sagalmireabukar@gmail.com>
2024-11-02 13:44:54 +0300

    accordion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React,  {useState} from 'react'
+import {IoIosArrowDown} from 'react-icons/io'
+import {IoIosArrowUp} from 'react-icons/io'
+import './App.css'
 
 function App() {
+
+  const [selected,setSelected]=useState(null)
+
+  const toggle =(i) =>{
+    if(selected === i){
+      return setSelected(null)
+    }
+
+    setSelected(i)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg'>
+    <div className='wrapper'>
+      <div className='accordion'>
+        {data.map((item, i)=>(
+          <div className='item'>
+            <div className='title' onClick={() =>toggle(i)}>
+                <h2>{item.select}</h2>
+                <span>{selected === i ? <IoIosArrowDown/> : <IoIosArrowUp/> }</span>
+
+            </div>
+            <div className={selected === i? 'content.show' : 'content' }>{item.option}</div>
+          </div>
+        ))}
+      </div>
     </div>
-  );
+    </div>
+    
+  )
 }
 
-export default App;
+const data=[
+  {
+  select: 'Option One',
+  option :
+  "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+     
+  } ,  
+  {
+  select: 'Option Two',
+  option :
+  "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+     
+  } ,  
+  {
+  select: 'Option Three',
+  option :
+  "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+     
+  } ,  
+  {
+  select: 'Option Four',
+  option :
+  "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit essecillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat nonproident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+     
+  }  
+]
+
+export default App
+
+
